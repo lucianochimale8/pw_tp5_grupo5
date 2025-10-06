5f88ae2505453554a57238cb58cfe776c235923f
lucianochimale8 <kchimale@gmail.com>
2025-10-06 19:43:17 -0300

    Estructura inicial con nav bar + cosas vistas en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './assets/views/Home';
+import Layout from './assets/views/Layout';
+import Game from './assets/views/Game';
+import AboutUs from './assets/views/AboutUs';
+import Error from './assets/views/Error';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hola</h1>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/Games" element={<Game />} />
+              <Route path="/AboutUs" element={<AboutUs />} />
+              <Route path="*" element={<Error />} />
+            </Route>
+          </Routes>
+        </Container>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/views/Error.jsx
@@ -0,0 +1,8 @@
+function Error(){
+
+    return(
+        <h1>ERROR</h1>
+    );
+}
+
+export default Error;
