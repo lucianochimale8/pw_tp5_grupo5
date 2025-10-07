57ca8b292a9bca485eefc4ae8ea6ab3a855133c8
lucianochimale8 <kchimale@gmail.com>
2025-10-07 13:50:42 -0300

    Mejorando estructura de la app y agregando un juego de Colores

--- src/assets/views/Game.jsx
+++ src/assets/views/Game.jsx
@@ -1,9 +1,10 @@
 import logo from '../img/me.png';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import Colores from "../component/Colores.jsx";
 
 function Game(){
-
+    
     const navegacion = useNavigate();
 
     const manejarClickImagen = () => {
@@ -13,9 +14,10 @@ function Game(){
     return(
         <>
             <h1> <Link to="/">Home</Link></h1>
-            <img src={logo} width="70%" alt="perrito" onClick={manejarClickImagen} />
+            <img src={logo} width="25%" alt="perrito" onClick={manejarClickImagen} />
+
+            <Colores />
         </>
-        
     );
 }
 
--- src/assets/views/Layout.jsx
+++ src/assets/views/Layout.jsx
@@ -1,38 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function Layout(){
 
     return(
         <>
-            <header>
-                <h1>Layout</h1>
+            <header className="bg-dark text-white text-center py-3">
+                <h1>Programación Web - Grupo 5</h1>
             </header>
-            
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">FPW</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <Link className="navbar-brand" to="/">FPW</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="/Games">Games</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="/AboutUs">About Us</a>
-                        </li>
-                    </ul>
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/games">Games</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/aboutus">About Us</Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
-            
-            <main>
-                <h1>Cuerpo</h1>
-                <Outlet></Outlet>
+            <main className="container py-4">
+                <Outlet />
             </main>
         </>
     );
