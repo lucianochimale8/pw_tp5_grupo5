a66cd81fb3c5ef939dda38a28fa694174f1c7bb4
Victor Caro <sbigtheo@gmail.com>
2025-10-09 19:40:00 -0300

    Caro Victor: realizacion de la estructura del juego de estrellas, falta pulir cositas

--- src/assets/views/Game.jsx
+++ src/assets/views/Game.jsx
@@ -1,7 +1,6 @@
 import logo from '../img/me.png';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import Colores from "../component/Colores.jsx";
 import Estrellas from '../component/Estrellas.jsx';
 
 function Game(){
@@ -17,11 +16,7 @@ function Game(){
             <h1> <Link to="/">Home</Link></h1>
             <img src={logo} width="25%" alt="perrito" onClick={manejarClickImagen} />
 
-            <Colores />
-
             <Estrellas />
-
-            
 
         </>
     );
