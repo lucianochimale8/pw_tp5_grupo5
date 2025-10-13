b24c289e1d8182f9da6431816c509019fa973fbf
Victor Caro <sbigtheo@gmail.com>
2025-10-13 13:11:36 -0300

    Caro Victor: funcion de audio cada vez que se hga click en la imagen de error

--- src/assets/views/Error.jsx
+++ src/assets/views/Error.jsx
@@ -1,21 +1,36 @@
+import { useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import errorImage from "../img/error.png";
+import Audio from "../audio/AtakeLoli.mp3";
 
 function Error() {
+  const audioRef = useRef(null);
+
+  const handleClick = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.volume = 0.5;
+      audioRef.current.play();
+    }
+  };
+
   return (
-    <div 
+    <div
       className=".container-xl"
-      style={{ overflow: "hidden" }}
+      style={{ overflow: "hidden", height: "100vh", width: "100vw" }}
     >
       <img
         src={errorImage}
         alt="Error"
+        onClick={handleClick}
         style={{
           width: "100%",
           height: "100%",
           objectFit: "contain",
+          cursor: "pointer",
         }}
       />
+      <audio ref={audioRef} src={Audio} preload="auto" />
     </div>
   );
 }
